c98d9b1aaa5bea1031f1d62986c8184ce225f69a
Rune K. Svendsen <runesvend@gmail.com>
2019-02-26 14:05:42 +0100

    Add ABook Aeson instance

--- src/CryptoVenues/Types/ABook.hs
+++ src/CryptoVenues/Types/ABook.hs
@@ -1,11 +1,24 @@
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE TypeApplications #-}
 module CryptoVenues.Types.ABook where
 
 import Prelude
 import CryptoVenues.Internal.Prelude
 import CryptoVenues.Fetch.MarketBook
 import OrderBook.Types
+-- LOL
+import qualified CryptoVenues.Venues            ()
+import           CryptoVenues.Fetch.MarketBook  (MarketBook)
+import qualified OrderBook.Types                as OB
 
+import qualified Money
+import qualified Data.Aeson                     as Json
+import           Data.Aeson                     ((.=), (.:))
+import           Data.Proxy                     (Proxy(..))
+import           GHC.TypeLits                   (KnownSymbol, SomeSymbol(..)
+                                                , symbolVal, someSymbolVal
+                                                )
+import           Data.Maybe                     (fromMaybe)
 
 -- | Just some order book
 data ABook =
@@ -53,3 +66,43 @@ abVenue
     -> Text
 abVenue _ =
     toS $ symbolVal (Proxy :: Proxy venue) :: Text
+
+
+-- TODO: Move below to "orderbook" library
+instance Json.ToJSON ABook where
+    toJSON (ABook ob) = case ob of
+        (_ :: OB.OrderBook venue base quote) -> Json.object
+            [ "bids"  .= Json.toJSON (OB.obBids ob)
+            , "asks"  .= Json.toJSON (OB.obAsks ob)
+            , "venue" .= Json.toJSON (symbolVal (Proxy :: Proxy venue))
+            , "base"  .= Json.toJSON (symbolVal (Proxy :: Proxy base))
+            , "quote" .= Json.toJSON (symbolVal (Proxy :: Proxy quote))
+            ]
+
+instance Json.FromJSON ABook where
+    parseJSON = Json.withObject "ABook" $ \obj -> do
+        venue <- obj .: "venue"
+        base  <- obj .: "base"
+        quote <- obj .: "quote"
+        case someSymbolVal base of
+            SomeSymbol (Proxy :: Proxy base) -> case someSymbolVal quote of
+                SomeSymbol (Proxy :: Proxy quote) -> do
+                    case marketBookVenue venue of
+                        Nothing -> Prelude.fail $ "Unknown venue: " ++ venue
+                        Just (MarketBookVenue (Proxy :: Proxy venue)) -> do
+                            buySide  <- obj .: "bids"
+                            sellSide <- obj .: "asks"
+                            let ob = OB.OrderBook buySide sellSide
+                            return $ ABook (ob :: OB.OrderBook venue base quote)
+
+data MarketBookVenue =
+    forall venue. MarketBook venue
+    => MarketBookVenue (Proxy venue)
+
+marketBookVenue :: String -> Maybe MarketBookVenue
+marketBookVenue "bitfinex" = Just . MarketBookVenue $ Proxy @"bitfinex"
+marketBookVenue "bittrex"  = Just . MarketBookVenue $ Proxy @"bittrex"
+marketBookVenue "binance"  = Just . MarketBookVenue $ Proxy @"binance"
+marketBookVenue "bitstamp" = Just . MarketBookVenue $ Proxy @"bitstamp"
+marketBookVenue "coinbase" = Just . MarketBookVenue $ Proxy @"coinbase"
+marketBookVenue _          = Nothing
