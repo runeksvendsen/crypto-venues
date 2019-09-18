e63805b56ce9c86e37b6c4da5d91f226f02c1a9e
Rune K. Svendsen <runesvend@gmail.com>
2019-09-18 17:21:12 +0200

    Bitfinex: switch to v2 API
    Fixes: https://github.com/runeksvendsen/crypto-venues/issues/1

--- src/CryptoVenues/Venues/Bitfinex.hs
+++ src/CryptoVenues/Venues/Bitfinex.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module CryptoVenues.Venues.Bitfinex
 ()
 where
 
 import CryptoVenues.Internal.CPrelude     hiding (asks)
-import OrderBook
+import qualified OrderBook.Types          as OB
 import CryptoVenues.Fetch
 import CryptoVenues.Types.Market
 import CryptoVenues.Venues.Common.ScientificOrder
@@ -19,65 +18,114 @@ import qualified Data.Vector  as Vec
 import qualified Data.Text as T
 
 
+-- NB: Two things you should know about Bitfinex' v2 API before proceeding:
+--
+--  1) Available markets are enumerated using the "tickers" API. This API also
+--      includes so-called "funding currencies" (which are not markets).
+--     "Funding currencies" are encoded in an array with a different length than that
+--      of the market tickers ("trading pair codes"), which means we have to manually
+--      grab the first element of this array (which always contains the symbol,
+--      regardless of whether it's a "trading pair code" (market) or "funding currency").
+--
+--  2) The sell and buy orders in an order book are encoded as a
+--      single list of orders, in which the buy orders are those with a positive
+--      quantity and the sell orders those with a negative quantity.
+
+apiUrl :: BaseUrl
+apiUrl = BaseUrl Https "api-pub.bitfinex.com" 443 ""
+
 instance EnumMarkets "bitfinex" where
-   allMarkets = DataSrc apiUrl clientM
+   allMarkets = DataSrc apiUrl (clientM $ Just "ALL")
       where
          clientM = SC.client (Proxy :: Proxy ApiMarkets)
 
 instance MarketBook "bitfinex" where
    marketBook apiSymbol = DataSrc apiUrl cm
-      where cm = clientM apiSymbol (Just 1000) (Just 1000)
-            clientM = SC.client (Proxy :: Proxy (Api base quote))
-   rateLimit = DataSrc apiUrl (return 1)
--- Orderbook rate limit: Ratelimit: 60 req/min (https://docs.bitfinex.com/v1/reference#rest-public-orderbook)
+      where cm = clientM apiSymbol "R0" (Just 100)
+            clientM = SC.client (Proxy :: Proxy (ApiBooks base quote))
+   rateLimit = DataSrc apiUrl (return 0.5)
+-- Orderbook rate limit: Ratelimit: 30 req/min (https://docs.bitfinex.com/v2/reference#rest-public-books)
 
-instance Json.FromJSON (SomeBook "bitfinex") where
+instance Json.FromJSON (OB.SomeBook "bitfinex") where
    parseJSON val =
-      let fromBook Book{..} = mkSomeBook
-                                    -- HACK: ignore zero-price buy orders from Bitfinex
-            <$> traverse parseOrder (Vec.filter nonZeroPrice bids)
-            <*> traverse parseOrder asks
-      in Json.parseJSON val >>= fromBook >>= either fail return
+      Json.parseJSON val >>= (either fail return . toSomeBook)
 
-data Book = Book
-   { bids :: Vector BitfinexOrder
-   , asks :: Vector BitfinexOrder
-   } deriving (Eq, Show, Generic)
+-- Take a Vector of orders -- as returned by the "books" API -- and
+--  turn them into a 'OB.SomeBook'.
+toSomeBook :: Vector BitfinexOrder -> Either String (OB.SomeBook "bitfinex")
+toSomeBook bookOrders = do
+    bids <- maybe (Left $ "Invalid buy order(s)" ++ show bookOrders) Right bidsM
+    asks <- maybe (Left $ "Invalid sell order(s)" ++ show bookOrders) Right asksM
+    OB.mkSomeBook bids asks
+  where
+    asksM = traverse toSellOrder $ Vec.filter (not . isBuyOrder) bookOrders
+    -- HACK: ignore zero-price buy orders from Bitfinex
+    bidsM = traverse toBuyOrder . Vec.filter nonZeroPrice . Vec.filter isBuyOrder $ bookOrders
+    -- NB: a sell order is encoded as an order with negative quantity
+    toSellOrder = toSomeOrder (* (-1))
+    toBuyOrder = toSomeOrder id
 
-data BitfinexOrder = BitfinexOrder
-   { price     :: QuotedScientific
-   , amount    :: QuotedScientific
-   , timestamp :: String
-   } deriving (Eq, Show, Generic)
+-- | (order_id, price, quantity)
+type BitfinexOrder = (Word64, Scientific, Scientific)
+
+isBuyOrder :: BitfinexOrder -> Bool
+isBuyOrder (_, _, qty) = qty > 0
 
 nonZeroPrice :: BitfinexOrder -> Bool
-nonZeroPrice BitfinexOrder{..} = price /= 0
+nonZeroPrice (_, price, _) = price /= 0
 
-instance Json.FromJSON Book
-instance Json.FromJSON BitfinexOrder
+-- |
+toSomeOrder
+   :: (Rational -> Rational)  -- ^ Modify quantity
+   -> BitfinexOrder
+   -> Maybe OB.SomeOrder
+toSomeOrder f (_, price, quantity) =
+   OB.mkSomeOrder (f $ toRational quantity) (toRational price)
 
-parseOrder :: BitfinexOrder -> Json.Parser SomeOrder
-parseOrder BitfinexOrder{..} = either fail return $ parseSomeOrderSci price amount
-
-apiUrl :: BaseUrl
-apiUrl = BaseUrl Https "api.bitfinex.com" 443 ""
-
-type Api base quote
-   = "v1"
+-- | Docs: https://docs.bitfinex.com/v2/reference#rest-public-books
+--   Example: https://api-pub.bitfinex.com/v2/book/tBTCUSD/R0?len=100
+type ApiBooks base quote
+   = "v2"
    :> "book"
    :> Capture "symbol" Text
-   :> QueryParam "limit_bids" Word
-   :> QueryParam "limit_asks" Word
-   :> Get '[JSON] (SomeBook "bitfinex")
+   :> Capture "precision" Text
+   :> QueryParam "len" Word
+   :> Get '[JSON] (OB.SomeBook "bitfinex")
 
--- | https://api.bitfinex.com/v1/symbols
+-- | Docs: https://docs.bitfinex.com/v2/reference#rest-public-tickers
+--   Example: https://api-pub.bitfinex.com/v2/tickers?symbols=ALL
 type ApiMarkets
-   = "v1"
-   :> "symbols"
+   = "v2"
+   :> "tickers"
+   :> QueryParam "symbols" Text
    :> Get '[JSON] (MarketList "bitfinex")
 
 instance Json.FromJSON (MarketList "bitfinex") where
-   parseJSON val = MarketList <$> Json.parseJSON val
+   parseJSON = Json.withArray "bitfinex MarketList" $ \vector ->
+      -- NB: We ignore symbols not starting with "t".
+      --     E.g.: "funding symbols", which start with "f".
+      let isTradingPair symbol = T.take 1 symbol == "t"
+          mkPairCodeVector =
+              traverse (Json.withArray "bitfinex ticker list" getPairCode)
+      in do
+         pairCodeVector <- Vec.filter isTradingPair <$> mkPairCodeVector vector
+         marketVector <- traverse (either fail return . parsePairCode) pairCodeVector
+         return $ MarketList (Vec.toList marketVector)
+
+parsePairCode :: Text -> Either String (Market "bitfinex")
+parsePairCode currPair =
+   let errorStr = "Invalid trading pair code: " ++ toS currPair
+   in maybe (Left errorStr) Right (fromPairCode currPair)
+
+-- | Get the trading pair code from a single item
+--    in the array returned by the "tickers" API
+getPairCode
+   :: Vec.Vector Json.Value
+   -> Json.Parser T.Text
+getPairCode vec =
+   -- symbol is always first item in the array
+   let getSymbol = (Vec.! 0)
+   in Json.withText "bitfinex trading pair code" return (getSymbol vec)
 
 -- | Bitfinex "(trading) pair code" string format (deduced through trial and error):
 -- Either (in descending priority):
@@ -89,21 +137,19 @@ instance Json.FromJSON (MarketList "bitfinex") where
 fromPairCode
    :: T.Text
    -> Maybe (Market "bitfinex")
-fromPairCode str
-   | T.length str == 6 =
-      mkMarket (T.toUpper $ T.take 3 str) (T.toUpper $ T.takeEnd 3 str)
-   | [baseSymbol, quoteSymbol] <- T.split (== ':') str =
+fromPairCode pairCode
+   | T.length cutPairCode == 6 =
+      mkMarket (T.toUpper $ T.take 3 cutPairCode) (T.toUpper $ T.takeEnd 3 cutPairCode)
+   | [baseSymbol, quoteSymbol] <- T.split (== ':') cutPairCode =
       mkMarket baseSymbol quoteSymbol
    | otherwise = Nothing
   where
+   -- The first character is 't' (for "Trading pair")
+   cutPairCode = discardFirstChar pairCode
+   discardFirstChar symbol = T.takeEnd (T.length symbol - 1) symbol
    mkMarket base quote =
       Just $ Market
             { miBase       = base
             , miQuote      = quote
-            , miApiSymbol  = str
+            , miApiSymbol  = pairCode
             }
-
-instance Json.FromJSON (Market "bitfinex") where
-   parseJSON = Json.withText "Bitfinex market" $ \currPair ->
-         let errorStr = "Invalid trading pair code: " ++ toS currPair
-         in maybe (fail errorStr) return (fromPairCode currPair)
