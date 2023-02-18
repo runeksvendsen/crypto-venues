292fd9846323834cf5b2a77536e9874bf873bf43
Rune K. Svendsen <runesvend@gmail.com>
2023-02-18 17:42:42 +0100

    Enable CI

---
+++ default.nix
@@ -0,0 +1,13 @@
+let
+  sources = import ./nix/sources.nix {};
+  haskellNix = import sources.haskellNix {};
+  pkgs = import
+    haskellNix.sources.nixpkgs-unstable
+    haskellNix.nixpkgsArgs;
+in pkgs.haskell-nix.project {
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "crypto-venues";
+    src = ./.;
+  };
+  compiler-nix-name = "ghc865"; # Not required for `stack.yaml` based projects.
+}
